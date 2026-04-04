64626c962538408bc634b6499dfe4e8b42596f0c
Moizez0406 <moizez0406@gmail.com>
2026-04-04 22:01:34 +0200

    Adding base server structure for rsa

--- load_input.py
+++ load_input.py
@@ -1,35 +1,27 @@
 def max_cut(filename):
-
     vertices = []
     edges = []
-
     mode = None
 
     with open(filename, "r") as f:
         for line in f:
-
             line = line.strip()
-
             if not line:
                 continue
             if line == "V":
                 mode = "V"
                 continue
-
             if line == "E":
                 mode = "E"
                 continue
-
             if mode == "V":
                 v = line.strip("()")
                 vertices.append(v)
-
             elif mode == "E":
                 part = line.split("-")
                 u = part[0].strip("()")
                 v = part[1].strip("()")
                 w = int(part[2])
-
                 edges.append(((u, v), w))
 
     return vertices, edges
