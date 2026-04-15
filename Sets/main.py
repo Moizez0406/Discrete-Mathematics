33b441f72871636051deaf2ea9eb68e10b1e6436
Dannen <moizez0406@gmail.com>
2026-04-15 23:56:36 +0200

    adding general operators

--- Sets/main.py
+++ Sets/main.py
@@ -1,17 +1,41 @@
 import sets
 
-a = [0,1,2,3]
-b = [2,3,4,5]
-c = [2,3,6,7]
-union = sets.union([a,b,c])
-intersection = sets.inter([a,b,c])
-universal = union.copy()
+x = ['a','b','c']
+a = [0,1,2,3,8]
+b = [2,3,4,5,9]
+c = [2,3,6,7, 8, 9]
+universal = [0,1,2,3,4,5,6,7,8,9]
+union = sets.union(a,b)
+intersection = sets.inter(a,b)
 complement_a = sets.complement(a, universal)
-
+cmp_b_respect_a = sets.relative_cmplt(a, b, universal)
+cartesian_product = sets.cartesian_product(a, b)
+#power_a = sets.power(a)
+power_x = sets.power(x)
 print(f"sets: ")
+print(f"x: {x}")
 print(f"a: {a}")
 print(f"b: {b}")
 print(f"c: {c}")
-print(f"Union? {union}")
-print(f"Intersection? {intersection}")
-print(f"Complement_a? {complement_a}")
+print(f"u: {universal}")
+print("------"*7)
+print("Syntax example")
+print(f"a u b: {union} (Lower U)")
+print(f"a n b: {intersection} (Lower N)")
+print(f"a': {complement_a}")
+print(f"b' respect to a (b-a): {cmp_b_respect_a}")
+print(f"a x b: ")
+print("P(x):")
+for element in power_x:
+    print("*********************")
+    for component in element:
+        for value in component:
+            print(value)
+print("------"*7)
+
+#while True:
+#    print("Enter q to exit")
+#    print("Enter add to add a new set")
+#    i = input("Enter expression: ")
+#    if i == 'q':
+#        break
--- Sets/sets.py
+++ Sets/sets.py
@@ -1,20 +1,15 @@
 
-def union(sets):
-    # assume we have more than 2 groups
-    new_set = sets[0].copy()
-    for i in range(1, len(sets)):
-        for element in sets[i]:
-            if element not in new_set:
-                new_set.append(element)
-    #new_set = sets[0]+sets[1]
+def union(a, b):
+    new_set = a.copy()
+    for element in b:
+        if element not in new_set:
+            new_set.append(element)
     return new_set
 
-def inter(sets):
-    # assume we have more than 2 groups
+def inter(a, b):
     new_set = []
-    for i in range(len(sets)-1):
-        for element in sets[i]:
-            if element in sets[i+1] and element not in new_set:
+    for element in a:
+        if element in b:
                 new_set.append(element)
     return new_set
 
@@ -24,4 +19,29 @@ def complement(set, universal):
         if u not in set:
             new_set.append(u)
     return new_set 
+
+def relative_cmplt(a, b, universal):
+    # We assume a, b E U set
+    new_set = complement(a, universal)
+    return inter(new_set, b)
+
+def cartesian_product(a, b):
+    new_sets = []
+    for element in a:
+        new_sets.append([element, x] for x in b])
+    return new_sets
+
+def power(a):
+    bin_set = [0,1]
+    new_sets = []
+    temp_sets = bin_set.copy()
+    for _ in range(len(a)):
+        new_sets = cartesian_product(temp_sets, bin_set)
+        temp_sets = new_sets.copy()
+
+    for value in temp_sets:
+        if value:
+           pass 
+
+    return new_sets
 
