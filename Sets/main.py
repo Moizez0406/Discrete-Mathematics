efa124ec30abb2e468baa697aa8bc8c76a810c81
Dannen <moizez0406@gmail.com>
2026-04-19 00:10:46 +0200

    added eval logic

--- Sets/main.py
+++ Sets/main.py
@@ -9,7 +9,7 @@ universal = [0,1,2,3,4,5,6,7,8,9]
 union = sets.union(a,b)
 intersection = sets.inter(a,b)
 complement_a = sets.complement(a, universal)
-cmp_b_respect_a = sets.relative_cmplt(a, b, universal)
+cmp_b_respect_a = sets.relative_cmplt(b, a, universal)
 cartesian_product = sets.cartesian_product(x, y)
 cartesian2 = sets.cartesian_product(cartesian_product, x, True)
 #power_a = sets.power(a)
@@ -20,20 +20,34 @@ print(f"y: {y}")
 print(f"a: {a}")
 print(f"b: {b}")
 print(f"c: {c}")
-print(f"u: {universal}")
+print(f"u: {universal}\n")
 print("------"*7)
-print("Syntax example")
-print(f"a u b: {union} (Lower U)")
-print(f"a n b: {intersection} (Lower N)")
+print("Operators syntax:\n")
+print("| Union                = u (lowercase U) |\n"+
+    "| Intersection         = n (lowercase N) |"+
+    "\n| Complement of x      = x'              |\n"+
+    "| x' with respect of y = x_y             |"+
+    "\n| Power of x           = Px              |\n")
+print("Syntax examples:\n")
+print(f"a u b: {union}")
+print(f"a n b: {intersection}")
 print(f"a': {complement_a}")
-print(f"b' respect to a (b-a): {cmp_b_respect_a}")
-print(f"a x b: {cartesian_product}")
-print(f"P(x): {power_x}")
+print(f"b' respect to a: {cmp_b_respect_a}")
+print(f"x x y: {cartesian_product}")
+print(f"P(x): {power_x}\n")
 print("------"*7)
 
-#while True:
-#    print("Enter q to exit")
-#    print("Enter add to add a new set")
-#    i = input("Enter expression: ")
-#    if i == 'q':
-#        break
+a = sets.build_set('a', [0,1,2,3,8])
+b = sets.build_set('b', [2,3,4,5,9])
+universal = sets.build_set('universal', [0,1,2,3,4,5,6,7,8,9])
+list_sets = [a,b,universal]
+
+print("Enter q to exit")
+print("Enter add to add a new set (NOT IMPLEMENTED)")
+while True:
+    expression = input("\nEnter expression: ")
+    result = sets.eval(expression.replace(" ", ""), list_sets)
+    print(f"result: {result}\n")
+    if expression == 'q':
+        break
+
--- Sets/sets.py
+++ Sets/sets.py
@@ -67,3 +67,53 @@ def power(a):
         return final_set
     return new_sets
 
+def build_set(name, values):
+    return [name, values]
+
+def eval(expression, list_sets):
+    # list_sets are in the form of built_sets
+    universal = None
+    for this_set in list_sets:
+        if this_set[0] == 'universal':
+            universal = this_set[1]
+
+    def read(left, rigth = True):
+        a = None
+        b = None
+
+        for this_set in list_sets:
+            if this_set[0] == left:
+                a = this_set[1]
+            if this_set[0] == rigth:
+                b = this_set[1]
+            if a and b and rigth:
+                return a, b
+        return a
+    for i in range(len(expression)):
+        if expression[i] == "(":
+            pass
+
+        elif expression[i] == "u":
+            a, b = read(expression[i-1],expression[i+1])
+            return union(a, b)
+
+        elif expression[i] == "n":
+            a, b = read(expression[i-1],expression[i+1])
+            return inter(a, b)
+
+        elif expression[i]== "'":
+            a = read(expression[i-1])
+            return complement(a, universal)
+
+        elif expression[i]== "_":
+            a, b = read(expression[i-1],expression[i+1])
+            return relative_cmplt(a, b, universal)
+
+        elif expression[i]== "x":
+            a, b = read(expression[i-1],expression[i+1])
+            return cartesian_product(a, b)
+
+        elif expression[i] == "P":
+            a = read(expression[i+1])
+            return power(a)
+    pass
